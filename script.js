8ec51988b7b2fbe7483be08201e04984405271ea
Varthag <vasilacheanton91@gmail.com>
2024-07-16 14:11:55 +0300

    formatting on calculation, page structure changes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const metric = document.getElementById("metric");
 const imperial = document.getElementById("imperial");
+const heroResult = document.querySelector(".hero-result");
 const heightMetric = document.querySelector(".height-metric");
 const weightMetric = document.querySelector(".weight-metric");
 const bmiResult = document.querySelector(".hero-result-number");
 const rangeMin = document.querySelector(".hero-result-range-min");
 const rangeMax = document.querySelector(".hero-result-range-max");
 const weightResult = document.querySelector(".hero-result-weight");
+const heroResultIntro = document.querySelector(".hero-result-intro");
+const heroResultHeader = document.querySelector(".hero-result-header");
+const heroResultContent = document.querySelector(".hero-result-content");
 const heightImperialFt = document.querySelector(".height-imperial-ft");
 const heightImperialIn = document.querySelector(".height-imperial-in");
 const weightImperialSt = document.querySelector(".weight-imperial-st");
 const weightImperialLbs = document.querySelector(".weight-imperial-lbs");
+const heroResultWelcome = document.querySelector(".hero-result-welcome");
 const bmiResultDescription = document.querySelector(".result-description-text");
 
 function unitChange(e) {
@@ -45,8 +50,17 @@ function updateBMI(input) {
     ? Number(weightImperialSt.value) * 14 + Number(weightImperialLbs.value)
     : null;
 
-  //if opposite input is valid, update BMI. Use BMI formula corresponding to the selected. Limited to 4 characters and 1 decimal.
-  if (input)
+  //if opposite input is valid, update BMI
+  if (input) {
+    //changing the intro strings when a calculation is done
+    heroResultIntro.textContent = `Your BMI suggests you are `;
+    heroResultWelcome.textContent = `Your BMI is...`;
+    heroResult.style.flexDirection = `row`;
+    heroResult.style.alignItems = `center`;
+    heroResultHeader.style.width = `50%`;
+    heroResultContent.style.width = `50%`;
+
+    //Use BMI formula corresponding to the selected. Limited to 4 characters and 1 decimal.
     bmiResult.textContent = metric.checked
       ? (Number(weightInput) / Math.pow(Number(heightInput) / 100, 2))
           .toFixed(1)
@@ -57,53 +71,56 @@ function updateBMI(input) {
           .slice(0, 4)
       : "0.0";
 
-  //If the last character is a decimal, remove it
-  if (bmiResult.textContent.charAt(3) === ".") {
-    bmiResult.textContent = bmiResult.textContent.slice(0, 3);
+    //If the last character is a decimal, remove it
+    if (bmiResult.textContent.charAt(3) === ".")
+      bmiResult.textContent = bmiResult.textContent.slice(0, 3);
+
+    //Infinity result is outputted as a string, after being sliced to 4 chars
+    if (bmiResult.textContent === "Infi") bmiResult.textContent = "0.0";
+
+    const bmiInterpreted = Number(bmiResult.textContent);
+
+    //change the description according to the BMI range
+    if (bmiInterpreted < 18.5) {
+      weightResult.textContent = "underweight. Your ideal weight is ";
+      bmiResultDescription.textContent = `A BMI below 18.5 is considered underweight. This can sometimes indicate underlying health conditions. If your BMI falls into this category, consult with a healthcare professional to determine the best course of action for you. They can help you develop a plan for healthy weight gain, which may involve dietary changes and exercise routines.`;
+    }
+
+    if (bmiInterpreted >= 18.5 && bmiInterpreted <= 24.9) {
+      weightResult.textContent = "a healthy weight. Your ideal weight is ";
+      bmiResultDescription.textContent = `A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.`;
+    }
+
+    if (bmiInterpreted >= 25 && bmiInterpreted < 29.9) {
+      weightResult.textContent = "overweight. Your ideal weight is ";
+      bmiResultDescription.textContent = `A BMI between 25 and 29.9 is considered overweight. While it doesn't necessarily indicate immediate health problems, it can increase your risk for certain conditions over time.  If your BMI falls into this category, consider talking to a doctor or registered dietitian about creating a weight management plan. This may include adjustments to your diet and exercise habits.`;
+    }
+
+    if (bmiInterpreted >= 30) {
+      weightResult.textContent = "obese. Your ideal weight is ";
+      bmiResultDescription.textContent = `A BMI over 30 is considered obese. This can significantly increase your risk for various health problems, including heart disease, stroke, type 2 diabetes, and some cancers. If your BMI falls into this category, consulting with a healthcare professional is crucial. They can help you develop a comprehensive plan for weight loss, which may include lifestyle changes, medication, or even surgery in some cases.`;
+    }
+
+    //calculating the ideal minimum and maximum weight, according to the selected units
+    const idealMin = metric.checked
+      ? (18.5 * Math.pow(Number(heightInput) / 100, 2)).toFixed(1)
+      : imperial.checked
+      ? ((18.5 * Math.pow(Number(heightInput), 2)) / 703).toFixed(1)
+      : "0.0";
+    const idealMax = metric.checked
+      ? (24.9 * Math.pow(Number(heightInput) / 100, 2)).toFixed(1)
+      : imperial.checked
+      ? ((24.9 * Math.pow(Number(heightInput), 2)) / 703).toFixed(1)
+      : "0.0";
+
+    //showing the ideal min and max weight, converted to lbs, if imperial is selected
+    rangeMin.textContent = metric.checked
+      ? `${idealMin}kgs - `
+      : `${(idealMin * 2.204).toFixed()}lbs - `;
+    rangeMax.textContent = metric.checked
+      ? `${idealMax}kgs`
+      : `${(idealMax * 2.204).toFixed()}lbs`;
   }
-
-  const bmiInterpreted = Number(bmiResult.textContent);
-
-  //change the description according to the BMI range
-  if (bmiInterpreted < 18.5) {
-    weightResult.textContent = "underweight";
-    bmiResultDescription.textContent = `A BMI below 18.5 is considered underweight. This can sometimes indicate underlying health conditions. If your BMI falls into this category, consult with a healthcare professional to determine the best course of action for you. They can help you develop a plan for healthy weight gain, which may involve dietary changes and exercise routines.`;
-  }
-
-  if (bmiInterpreted >= 18.5 && bmiInterpreted <= 24.9) {
-    weightResult.textContent = "a healthy weight";
-    bmiResultDescription.textContent = `A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.`;
-  }
-
-  if (bmiInterpreted >= 25 && bmiInterpreted < 29.9) {
-    weightResult.textContent = "overweight";
-    bmiResultDescription.textContent = `A BMI between 25 and 29.9 is considered overweight. While it doesn't necessarily indicate immediate health problems, it can increase your risk for certain conditions over time.  If your BMI falls into this category, consider talking to a doctor or registered dietitian about creating a weight management plan. This may include adjustments to your diet and exercise habits.`;
-  }
-
-  if (bmiInterpreted >= 30) {
-    weightResult.textContent = "obese";
-    bmiResultDescription.textContent = `A BMI over 30 is considered obese. This can significantly increase your risk for various health problems, including heart disease, stroke, type 2 diabetes, and some cancers. If your BMI falls into this category, consulting with a healthcare professional is crucial. They can help you develop a comprehensive plan for weight loss, which may include lifestyle changes, medication, or even surgery in some cases.`;
-  }
-
-  //calculating the ideal minimum and maximum weight, according to the selected units
-  const idealMin = metric.checked
-    ? (18.5 * Math.pow(Number(heightInput) / 100, 2)).toFixed(1)
-    : imperial.checked
-    ? ((18.5 * Math.pow(Number(heightInput), 2)) / 703).toFixed(1)
-    : "0.0";
-  const idealMax = metric.checked
-    ? (24.9 * Math.pow(Number(heightInput) / 100, 2)).toFixed(1)
-    : imperial.checked
-    ? ((24.9 * Math.pow(Number(heightInput), 2)) / 703).toFixed(1)
-    : "0.0";
-
-  //showing the ideal min and max weight, converted to lbs, if imperial is selected
-  rangeMin.textContent = metric.checked
-    ? `${idealMin}kgs`
-    : `${(idealMin * 2.204).toFixed()}lbs`;
-  rangeMax.textContent = metric.checked
-    ? `${idealMax}kgs`
-    : `${(idealMax * 2.204).toFixed()}lbs`;
 }
 
 //when typing either of the values, the BMI will update when both values are filled in
